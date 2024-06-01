180c74240dff302d46b7373ddeb0b92074582fb7
minthukha <minthukha1239788@gmail.com>
2024-06-01 21:01:45 +0630

    -

--- InventoryManagementSystemApi/Features/InventoryItem/InventoryItemController.cs
+++ InventoryManagementSystemApi/Features/InventoryItem/InventoryItemController.cs
@@ -10,26 +10,5 @@
         {
             _bl_inventoryItem = bl_inventoryItem;
         }
-
-        //[HttpGet("GetAllItem")]
-        //public async Task<IActionResult> GetAllItem()
-        //{
-        //    var lst = await  _bl_inventoryItem.GetAllItem();
-        //    return Ok(lst);
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> GetAllItem1()
-        //{
-        //    try
-        //    {
-        //        var model = await _bl_inventoryItem.GetAllItem();
-        //        return Ok(model);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest();
-        //    }
-        //}
     }
 }
