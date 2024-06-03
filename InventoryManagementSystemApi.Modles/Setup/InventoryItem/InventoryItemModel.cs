260e63dd122441947ccd34bf714e3f281d9b8a73
minthukha <minthukha1239788@gmail.com>
2024-06-04 00:26:41 +0630

    [ Added ] - Model

--- InventoryManagementSystemApi.Modles/Setup/InventoryItem/InventoryItemModel.cs
+++ InventoryManagementSystemApi.Modles/Setup/InventoryItem/InventoryItemModel.cs
@@ -2,10 +2,9 @@
 {
     public class InventoryItemModel
     {
-        public int ItemId { get; set; }
         public string ItemName { get; set; }
         public string ItemCategory { get; set; }
-        public string ItemAmount { get; set; }
         public decimal ItemPrice { get; set; }
+        public string ItemAmount { get; set; }
     }
 }
