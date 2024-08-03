98c09cd1f332e7979ca6a988320b7f19197f7c2e
minthukha <minthukha1239788@gmail.com>
2024-08-04 03:13:32 +0630

    fix : changeModel

--- InventoryManagementSystemApi.Modles/ChangeModel.cs
+++ InventoryManagementSystemApi.Modles/ChangeModel.cs
@@ -1,63 +1,14 @@
-//using Microsoft.VisualBasic;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using InventoryManagementSystemApi.DbService.Db;
+using InventoryManagementSystemApi.Modles.Setup.Category;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace InventoryManagementSystemApi.Modles;
+namespace InventoryManagementSystemApi.Modles;
 
-//public static class ChangeModel
-//{
-//    #region InventoryItem
-
-//    public static InventoryItemModel Change(this TblItem dataModel)
-//    {
-//        var item = new InventoryItemModel
-//        {
-//            ItemName = dataModel.ItemName,
-//            ItemCategory = dataModel.ItemCategory,
-//            ItemAmount = dataModel.IteamAmount,
-//            ItemPrice = dataModel.ItemPrice,
-//        };
-//        return item;
-//    }
-
-//    public static TblItem Change(this InventoryItemModel dataModel)
-//    {
-//        var item = new TblItem
-//        {
-//            ItemName = dataModel.ItemName,
-//            ItemCategory = dataModel.ItemCategory,
-//            ItemPrice = dataModel.ItemPrice,
-//            IteamAmount = dataModel.ItemAmount,
-//        };
-//        return item;
-//    }
-
-//    #endregion
-
-//    #region InventoryCategory
-
-//    public static InventoryCategoryModel Change(this TblCategory dataModel)
-//    {
-//        var item = new InventoryCategoryModel
-//        {
-//            CategoryId = dataModel.CategoryId,
-//            CategoryName = dataModel.CategoryName,
-//        };
-//        return item;
-//    }
-
-//    public static TblCategory Change(this InventoryCategoryModel dataModel)
-//    {
-//        var item = new TblCategory
-//        {
-//            CategoryId = dataModel.CategoryId,
-//            CategoryName = dataModel.CategoryName,
-//        };
-//        return item;
-//    }
-
-//    #endregion
-//}
+public static class ChangeModel
+{
+}
