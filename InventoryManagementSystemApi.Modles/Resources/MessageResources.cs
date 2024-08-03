f3edb41399014d4b0931662c028e10fed04a2f9d
minthukha <minthukha1239788@gmail.com>
2024-08-04 03:13:05 +0630

    feate : messageResoucre

---
+++ InventoryManagementSystemApi.Modles/Resources/MessageResources.cs
@@ -0,0 +1,21 @@
+﻿namespace InventoryManagementSystemApi.Modles.Resources;
+
+public static class MessageResources
+{
+    public static string Success { get; } = "Success";
+    public static string Fail { get; } = "Fail";
+    public static string SaveSuccess { get; } = "Successfully Save.";
+    public static string SaveFail { get; } = "Fail to Save.";
+    public static string NotFound { get; } = "No Data Found";
+    public static string UpdateSuccess { get; } = "Successfully Update.";
+    public static string UpdateFail { get; } = "Fail to Update.";
+    public static string DeleteSuccess { get; } = "Successfully Delete.";
+    public static string DeleteFail { get; } = "Fail to Delete.";
+    public static string Duplicate { get; } = "Duplicate Data.";
+    public static string InvalidUser { get; } = "Invalid User.";
+    public static string InvalidAccessToken { get; } = "Invalid Access Token.";
+    public static string UserNameRequire { get; } = "User Name is required.";
+    public static string PassowrdRequire { get; } = "Passowrd is required.";
+    public static string RefreshTokenRequire { get; } = "Refresh Token is required.";
+    public static string AccessTokenRequire { get; } = "Access Token is required.";
+}
