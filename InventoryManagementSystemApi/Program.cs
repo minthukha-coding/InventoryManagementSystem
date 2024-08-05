8f4f9ee36d10b05b6292de2fd648fc87e4a15f99
minthukha <minthukha1239788@gmail.com>
2024-08-05 21:51:53 +0630

    feat : test deploy

--- InventoryManagementSystemApi/Program.cs
+++ InventoryManagementSystemApi/Program.cs
@@ -13,11 +13,11 @@ builder.Services.AddServices(builder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
