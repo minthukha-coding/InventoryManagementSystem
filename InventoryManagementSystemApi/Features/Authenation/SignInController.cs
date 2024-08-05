f3abfe1c32134e88e330deb8d4a9afad571e1757
minthukha <minthukha1239788@gmail.com>
2024-08-06 00:05:47 +0630

    feat : jwtToken

--- InventoryManagementSystemApi/Features/Authenation/SignInController.cs
+++ InventoryManagementSystemApi/Features/Authenation/SignInController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InventoryManagementSystemApi.Modles.Setup.Authenation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace InventoryManagementSystemApi.Features.Authenation;
 
@@ -7,4 +11,26 @@ namespace InventoryManagementSystemApi.Features.Authenation;
 [ApiController]
 public class SignInController : ControllerBase
 {
+    [HttpPost("signIn")]
+    public async Task<IActionResult> SignIn([FromBody] UserModel reqModel)
+    {
+        if (reqModel.UserName != "test" || reqModel.UserPassword != "password")
+            return Unauthorized();
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes("YourSecretKeyHere");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, reqModel.UserName)
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        // Return the token
+        return Ok(new { Token = tokenString });
+    }
 }
--- InventoryManagementSystemApi/Program.cs
+++ InventoryManagementSystemApi/Program.cs
@@ -1,6 +1,29 @@
 using InventoryManagementSystemApi;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//jwt 
+var key = Encoding.ASCII.GetBytes("HQfsdfQ@C1"); // Use a secret key for encoding the token
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 // Add services to the container.
 
@@ -13,16 +36,16 @@ builder.Services.AddServices(builder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllers();
 
