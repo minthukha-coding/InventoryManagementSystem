3ce39ee7882a8ebd5ac76aaaa8c3605b82e80eda
minthukha <minthukha1239788@gmail.com>
2024-08-07 01:39:38 +0630

    fix : model

--- InventoryManagementSystemApi.Modles/Setup/Authenation/SignInModel.cs
+++ InventoryManagementSystemApi.Modles/Setup/Authenation/SignInModel.cs
@@ -9,7 +9,7 @@ namespace InventoryManagementSystemApi.Modles.Setup.Authenation;
 public class SignInRequestModel
 {
     public string UserName { get; set; }
-    public string UserPassword { get; set; }
+    public string HashPassword { get; set; }
 }
 //public class SignInResponseModel
 //{
